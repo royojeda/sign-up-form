53ccb15330fdf27700a995c3a970bb5be75f2c11
Dennis Ojeda <royojeda1@gmail.com>
2022-06-24 17:36:38 +0800

    Update the form validation

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 password = document.querySelector('#password');
 passwordConfirmation = document.querySelector('#passwordConfirmation');
 passwordError = document.querySelector('input + span');
+button = document.querySelector('.bottom button');
+email = document.querySelector('#email');
 
+button.disabled = true;
 passwordConfirmation.addEventListener('input', checkMatch);
+email.addEventListener('input', checkMatch);
 
 function checkMatch() {
   if (passwordConfirmation.value != '') {
@@ -11,12 +15,22 @@ function checkMatch() {
       password.style['border-color'] = 'red';
       passwordConfirmation.style['border-color'] = 'red';
       passwordError.style['visibility'] = 'visible'
+      button.disabled = true;
     } else {
+      password.style['border-color'] = 'lightgray';
+      passwordConfirmation.style['border-color'] = 'lightgray';
+      passwordError.style['visibility'] = 'hidden'
+      if (email.checkValidity()) {
+        button.disabled = false;
+      } else {
+        button.disabled = true;
+      }
+    }
+  } else {
     password.style['border-color'] = 'lightgray';
     passwordConfirmation.style['border-color'] = 'lightgray';
     passwordError.style['visibility'] = 'hidden'
-    }
-  } else {
     password.removeEventListener('input', checkMatch);
+    button.disabled = true;
   }
 }
